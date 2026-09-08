1e7caeb3d8f94f92f4ac7f972bc47e735bfd5449
Luca Nicolini <mithosk85@gmail.com>
2026-09-09 00:07:07 +0200

    Update ai_client.py

--- core/src/ai_client.py
+++ core/src/ai_client.py
@@ -1,4 +1,5 @@
 from google.genai import Client
+from google.genai.types import Content, Part
 
 EMBEDDING_MODEL = "gemini-embedding-2"
 
@@ -7,7 +8,9 @@ class AIClient:
     def __init__(self, api_key: str):
         self._client = Client(api_key=api_key)
 
-    def embed(self, contents: list[str]) -> list[list[float]]:
+    def embed(self, texts: list[str]) -> list[list[float]]:
+        contents = [Content(parts=[Part.from_text(text=text)]) for text in texts]
+
         embed_response = self._client.models.embed_content(
             contents=contents, model=EMBEDDING_MODEL
         )

101fb6b6c1301dffd8732d0e10d796db38d97cdb
Luca Nicolini <mithosk85@gmail.com>
2026-09-23 13:05:26 +0200

    Update chunker.py

--- core/src/chunker.py
+++ core/src/chunker.py
@@ -1,134 +1,113 @@
+# UNO + OTTO
 import re
 import uuid
+from point import Point
 from pathlib import Path
-from google.genai import Client
-from qdrant_client import QdrantClient
-from google.genai.types import EmbedContentConfig, Content, Part
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from qdrant_client.models import Distance, PointStruct, VectorParams
+from db_client import DBClient
+from ai_client import AIClient
+from langchain_text_splitters import RecursiveCharacterTextSplitter, Language
 
+# DUE + NOVE
 CHUNK_BATCH_SIZE = 100
 CHUNK_OVERLAP = 150
 CHUNK_SIZE = 1000
 DB_COLLECTION_NAME = "knowledge"
-DB_VECTOR_SIZE = 3072
-EMBEDDING_MODEL = "gemini-embedding-2"
 MIN_CHUNK_VALUE_LEN = 30
-QUERY_POINTS_LIMIT = 5
-QUERY_POINTS_SCORE_THRESHOLD = 0.45
 
 
-def clean_text(text: str) -> str:
-    return re.sub(r"\n{3,}", "\n\n", text).strip()
+class Chunker:
 
+    #TRE
+    def __init__(self, db_client: DBClient, ai_client: AIClient):
+        self.db_client = db_client
+        self.ai_client = ai_client
 
-def save_chunk_batch(chunk_batch: list[dict], qdrant_client: QdrantClient, gemini_client: Client):
-    contents = [
-        Content(
-            parts=[
-                Part.from_text(
-                    text=f"{chunk["pattern"]}\n\n{chunk["value"]}"
-                )
+    #QUATTRO
+    @staticmethod
+    def _clean_text(text: str) -> str:
+        return re.sub(r"\n{3,}", "\n\n", text).strip()
+
+    #CINQUE
+    def _save_chunk_batch(self, chunk_batch: list[dict]):
+        vectors = self.ai_client.embed(
+            texts=[
+                f"{chunk["pattern"]}\n\n{chunk["value"]}"
+                for chunk in chunk_batch
             ]
         )
-        for chunk in chunk_batch
-    ]
 
-    embed_response = gemini_client.models.embed_content(
-        model=EMBEDDING_MODEL,
-        contents=contents,
-        config=EmbedContentConfig(
-            output_dimensionality=DB_VECTOR_SIZE
+        self.db_client.upsert(
+            points=[
+                Point(
+                    id=str(uuid.uuid4()),
+                    chunk=chunk["value"],
+                    vector=vector
+                )
+                for chunk, vector in zip(chunk_batch, vectors)
+            ],
+            collection_name=DB_COLLECTION_NAME,
         )
-    )
 
-    points = [
-        PointStruct(
-            id=uuid.uuid4(),
-            vector=[float(value) for value in (embedding.values or [])],
-            payload={
-                "pattern": chunk["pattern"],
-                "value": chunk["value"]
-            }
+        print(f"Saved {len(chunk_batch)} chunks")
+
+    #SEI
+    def refresh_chunks(self, directory_path: str):
+        print(f"Start of refreshing chunks from {directory_path}")
+
+        self.db_client.clean(DB_COLLECTION_NAME)
+        print(f"Cleaned collection {DB_COLLECTION_NAME}")
+
+        text_splitter = RecursiveCharacterTextSplitter.from_language(
+            language=Language.MARKDOWN,
+            chunk_overlap=CHUNK_OVERLAP,
+            chunk_size=CHUNK_SIZE,
         )
-        for chunk, embedding in zip(chunk_batch, embed_response.embeddings or [])
-    ]
 
-    qdrant_client.upsert(
-        collection_name=DB_COLLECTION_NAME,
-        points=points
-    )
+        chunk_batch: list[dict] = []
 
-    print(f"Saved {len(chunk_batch)} chunks")
+        for file_path in Path(directory_path).rglob("*.md"):
+            file_text = file_path.read_text(encoding="utf-8")
+            cleaned_file_text = self._clean_text(file_text)
+            splitted_file_text = text_splitter.split_text(cleaned_file_text)
 
+            for file_text_part in splitted_file_text:
+                stripped_file_text_part = file_text_part.strip()
 
-def refresh_chunks(directory_path: str, qdrant_client: QdrantClient, gemini_client: Client):
-    print(f"Start of refreshing chunks from {directory_path}")
+                if len(stripped_file_text_part) >= MIN_CHUNK_VALUE_LEN:
+                    chunk_batch.append({
+                        "pattern": file_path.relative_to(directory_path),
+                        "value": stripped_file_text_part
+                    })
 
-    qdrant_client.delete_collection(DB_COLLECTION_NAME)
-    qdrant_client.create_collection(
-        collection_name=DB_COLLECTION_NAME,
-        vectors_config=VectorParams(
-            size=DB_VECTOR_SIZE,
-            distance=Distance.COSINE
+                    if len(chunk_batch) == CHUNK_BATCH_SIZE:
+                        self._save_chunk_batch(chunk_batch)
+                        chunk_batch.clear()
+
+        if chunk_batch:
+            self._save_chunk_batch(chunk_batch)
+
+        print(f"End of refreshing chunks from {directory_path}\n")
+
+    #SETTE
+    def generate_text(self, query: str) -> str:
+        vectors = self.ai_client.embed(
+            texts=[self._clean_text(query)]
         )
-    )
 
-    print(f"Cleaned collection {DB_COLLECTION_NAME}")
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_overlap=CHUNK_OVERLAP,
-        chunk_size=CHUNK_SIZE
-    )
-
-    chunk_batch: list[dict] = []
-
-    for file_path in Path(directory_path).rglob("*.md"):
-        file_text = file_path.read_text(encoding="utf-8")
-        cleaned_file_text = clean_text(file_text)
-        splitted_file_text = text_splitter.split_text(cleaned_file_text)
-
-        for file_text_part in splitted_file_text:
-            stripped_file_text_part = file_text_part.strip()
-
-            if len(stripped_file_text_part) >= MIN_CHUNK_VALUE_LEN:
-                chunk_batch.append({
-                    "pattern": file_path.relative_to(directory_path),
-                    "value": stripped_file_text_part
-                })
-
-                if len(chunk_batch) == CHUNK_BATCH_SIZE:
-                    save_chunk_batch(chunk_batch, qdrant_client, gemini_client)
-                    chunk_batch.clear()
-
-    if chunk_batch:
-        save_chunk_batch(chunk_batch, qdrant_client, gemini_client)
-
-    print(f"End of refreshing chunks from {directory_path}\n")
-
-
-def generate_text(query: str, qdrant_client: QdrantClient, gemini_client: Client) -> str:
-    cleaned_query = clean_text(query)
-
-    embed_response = gemini_client.models.embed_content(
-        model=EMBEDDING_MODEL,
-        contents=cleaned_query,
-        config=EmbedContentConfig(
-            output_dimensionality=DB_VECTOR_SIZE
+        qdrant_response = self.db_client.search(
+            vector=vectors[0],
+            collection_name=DB_COLLECTION_NAME
         )
-    )
 
-    qdrant_response = qdrant_client.query_points(
-        collection_name=DB_COLLECTION_NAME,
-        query=[float(value) for value in (embed_response.embeddings[0].values or [])],
-        limit=QUERY_POINTS_LIMIT,
-        score_threshold=QUERY_POINTS_SCORE_THRESHOLD,
-        with_payload=True
-    )
+        values = []
+        for point in qdrant_response:
+            values.append(point.chunk)
 
-    values = []
-    for point in qdrant_response.points:
-        if point.payload:
-            values.append(point.payload["value"])
+        return "\n\n".join(values)
 
-    return "\n\n".join(values)
+# DIECI
+def ciao(knowledge_dir:str, db_client: DBClient, ai_client: AIClient):
+    x=Chunker(db_client=db_client, ai_client=ai_client)
+    x.refresh_chunks(knowledge_dir)
+    y = x.generate_text("karate tiger")
+    print(f"Generated text: {y}")
